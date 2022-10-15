f57dd2e8358a9dcb1526743b78a4721627749235
Ruhan Potgieter <ruhan@sietechnn.co.za>
2022-10-15 14:45:53 +0200

    yes

--- APF(Alien Plant Finder)/APF(Alien Plant Finder).Android/obj/Debug/120/android/src/crc64f85c10fa6191e76c/MainActivity.java
+++ APF(Alien Plant Finder)/APF(Alien Plant Finder).Android/obj/Debug/120/android/src/crc64f85c10fa6191e76c/MainActivity.java
@@ -20,16 +20,18 @@ public class MainActivity
 	public MainActivity ()
 	{
 		super ();
-		if (getClass () == MainActivity.class)
+		if (getClass () == MainActivity.class) {
 			mono.android.TypeManager.Activate ("APF_Alien_Plant_Finder_.Droid.MainActivity, APF(Alien Plant Finder).Android", "", this, new java.lang.Object[] {  });
+		}
 	}
 
 
 	public MainActivity (int p0)
 	{
 		super (p0);
-		if (getClass () == MainActivity.class)
+		if (getClass () == MainActivity.class) {
 			mono.android.TypeManager.Activate ("APF_Alien_Plant_Finder_.Droid.MainActivity, APF(Alien Plant Finder).Android", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+		}
 	}
 
 
